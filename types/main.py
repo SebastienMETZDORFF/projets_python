267bbbd11344b8d5114f6aa71c00e8c097854a0b
SebastienMETZDORFF <sebmetzdorff@gmail.com>
2025-06-24 16:30:49 +0200

    Je classe des données avec les types de données

---
+++ types/main.py
@@ -0,0 +1,17 @@
+# Création d'une variable nom contenant mon nom sous forme de chaîne de caractères
+nom = "Sébastien"
+
+# Création d'une variable age contenant mon âge sous forme d'un entier
+age = 35
+
+# Création d'une variable taille contenant ma taille sous forme d'un nombre à virgule flottante
+taille = 1.78
+
+# Création d'une variable est_etudiant contenant une valeur booléenne et indiquant si je suis étudiant ou non
+est_etudiant = False
+
+# Afficher les valeurs des variables précédemment définies et leur type
+print(f"nom = {nom}, type = {type(nom)}")
+print(f"age = {age}, type = {type(age)}")
+print(f"taille = {taille}, type = {type(taille)}")
+print(f"est_etudiant = {est_etudiant}, type = {type(est_etudiant)}")
