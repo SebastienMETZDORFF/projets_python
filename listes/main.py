638cedb8f5fc561fd4a666b680c9544cd52d03c8
SebastienMETZDORFF <sebmetzdorff@gmail.com>
2025-06-24 17:10:36 +0200

    Enregistrer des groupes de données avec les listes

---
+++ listes/main.py
@@ -0,0 +1,24 @@
+# Création d'une liste fruits
+fruits = ["pomme", "banane", "orange"]
+print(fruits)
+
+# Ajouter "kiwi" à la liste fruits
+fruits.append("kiwi")
+print(fruits)
+
+# Supprimer "orange" de la liste fruits
+fruits.remove("orange")
+print(fruits)
+
+# Modifier le deuxième élément
+fruits[1] = "ananas"
+print(fruits)
+
+# Afficher la longueur de la liste
+print(len(fruits))
+
+# Trier la liste par ordre alphabétique
+fruits.sort()
+
+# Afficher la liste après le tri
+print(fruits)
