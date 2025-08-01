30f659b72e4fcaf1f0ada6cb73ead3094cd04a4b
SebastienMETZDORFF <sebmetzdorff@gmail.com>
2025-08-01 14:34:32 +0200

    J'importe un module Python

---
+++ modules/main.py
@@ -0,0 +1,6 @@
+from operations import addition, multiplication
+
+if __name__ == "__main__":
+    somme = addition(3, 5)
+    produit = multiplication(8, 2)
+    print(f"Somme = {somme}, produit = {produit}")
